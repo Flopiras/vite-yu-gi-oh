0485b8e4dc118a81b7f717fd8e222aa82d55d167
Flopiras <Flopiras@yahoo.it>
2023-07-03 14:48:21 +0200

    renamed data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script>
 import AppMain from './components/AppMain.vue';
 import axios from 'axios';
+import { store } from '../src/data/store';
+
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
 
 export default {
   components: { AppMain },
+  created() {
+    axios.get(endpoint).then(res => {
+      store.pokemons = res.data.results;
+    })
+  },
 
 }
 </script>
