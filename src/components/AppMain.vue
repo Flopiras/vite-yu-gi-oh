66fa744be7d35e0531768b8cab6cc404a1c8706d
Flopiras <Flopiras@yahoo.it>
2023-07-03 15:35:52 +0200

    add CharacterCard

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,21 @@
 <script>
+import { store } from '../../src/data/store';
+import CharacterCard from './CharacterCard.vue';
 
+export default {
+    components: { CharacterCard },
+
+    data() {
+        return { store }
+    }
+}
 </script>
 
 <template>
     <div class="row row-cols-2 row-cols-md-4 row-cols-lg-6">
-
+        <div class="col" v-for="pokemon in store.pokemons" :key="pokemon.id">
+            <CharacterCard :number="pokemon.number" :name="pokemon.name" :type="pokemon.type1" :url="pokemon.imageUrl" />
+        </div>
     </div>
 </template>
 
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    props: {
+        number: Number,
+        name: String,
+        type: String,
+        url: String
+    }
+
+}
+</script>
+
+<template>
+    <div class="character-card text-center">
+        <img :src="url" :alt="name">
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
