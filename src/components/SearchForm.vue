b58721f788c1bffc006e54887a5ecba22e55a4b7
Flopiras <Flopiras@yahoo.it>
2023-07-04 14:49:04 +0200

    add select

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    data() {
+        return {
+            types: [
+                "Bug",
+                "Dark",
+                "Dragon",
+                "Electric",
+                "Fairy",
+                "Fighting",
+                "Fire",
+                "Flying",
+                "Ghost",
+                "Grass",
+                "Ground",
+                "Ice",
+                "Normal",
+                "Poison",
+                "Psychic",
+                "Rock",
+                "Steel",
+                "Water"
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+
+        <form>
+            <select class="form-select my-4">
+                <option selected>Select type</option>
+                <option v-for="type in types">{{ type }}</option>
+            </select>
+        </form>
+    </div>
+</template>
